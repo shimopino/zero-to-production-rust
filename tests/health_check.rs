cb299bbc2f6295c6feb044fe3a8a05c34b66d091
shimopino <shimoke4869@gmail.com>
2023-01-03 19:15:02 +0900

    仕様を明らかにした後で、失敗する結合テストを追加

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,6 +20,59 @@ async fn health_check_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spwan_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    // パーセントエンコーディングのため、空白は %20 でエンコードされている
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencode")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let address = spwan_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencode")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Arrange
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // テスト失敗時に表示するメッセージをカスタマイズする
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        )
+    }
+}
+
 // アプリケーションコードに依存している箇所は唯一ここだけ
 // 内容はほとんど src/main.rs に記載している内容とほとんど同じである
 fn spwan_app() -> String {
