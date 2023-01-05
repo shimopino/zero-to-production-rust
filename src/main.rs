949878260a269602804822ccddfc2b70cf74b820
shimopino <shimoke4869@gmail.com>
2023-01-06 00:19:51 +0900

    configクレートを使用して、アプリケーションの設定を反映する

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // ポートの0番を指定すると、OSから使用可能なポート番号が指定される
-    let address = TcpListener::bind("127.0.0.1:8000")?;
+    // configurationファイルを読めなければパニックさせる
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = TcpListener::bind(address)?;
     run(address)?.await
 }
