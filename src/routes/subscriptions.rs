fc6bd063cc05571151f3fa466aff4212d32623e6
shimopino <shimoke4869@gmail.com>
2023-01-06 03:59:27 +0900

    POSTリクエストのハンドラに対して、ログを出力するための設定を追加

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -15,6 +15,20 @@ pub async fn subscribe(
     // この機能を使って依存性の注入を実現できる
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    // もしも何かしら調査が必要になった場合に備えて、情報はログに出力するようにする
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+    // ネットワーク経由で外部に接続する箇所は環境や状況に応じてレスポンス時間などが変化する
+    // そのためしっかりとログを出力しておく必要がある
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -28,9 +42,19 @@ pub async fn subscribe(
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
