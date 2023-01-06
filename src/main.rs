fdb644523eed48954ff5ad2ea0db3a87c79ea63a
shimopino <shimoke4869@gmail.com>
2023-01-07 01:39:22 +0900

    actix-webのイベントもログ出力を行うように設定を追加

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Registry};
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_sbscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_sbscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     // configurationファイルを読めなければパニックさせる
     let configuration = get_configuration().expect("Failed to read configuration");
