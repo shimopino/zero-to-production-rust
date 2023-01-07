957579498f5a70b474f2da938389b4e66cf7ec10
shimopino <shimoke4869@gmail.com>
2023-01-07 17:18:55 +0900

    instrumentを使用してロジック部分とログ収集部分を分離した

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,28 +9,34 @@ pub struct FormData {
     name: String,
 }
 
+// ログは関数宣言を修飾するために使用され、関数本体は本当に記述したいロジックのみに集中できる
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     // actix-webは type-map 機能を使って同じ型に紐づく値を注入する
     // この機能を使って依存性の注入を実現できる
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    // もしも何かしら調査が必要になった場合に備えて、情報はログに出力するようにする
-    let request_id = Uuid::new_v4();
-    // Spansを作成してログを出力する
-    // Spanでは key-value な値としてログを登録することで構造化を行う
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id, // % をつけることで Display 実装を使うようにしている
-        form.email,
-        form.name
-    );
-    //
-    let _request_span_guard = request_span.enter();
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    // enterは実行しない。 .instrument で登録する
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -41,16 +46,12 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    // await を呼び出す前に tracing を紐づける
-    // これで Future が完了するまでに Executor によって何回ポーリングされたのかわかる
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
