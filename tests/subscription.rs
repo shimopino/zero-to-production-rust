279178f4c48ecae36fae689a20d21bae8583c08f
shimopino <shimoke4869@gmail.com>
2023-04-17 02:21:37 +0900

    feat: テストディレクトリ内部でも関数の共通化

--- tests/subscription.rs
+++ tests/subscription.rs
@@ -1,54 +1,12 @@
 use axum::{
     body::Body,
     http::{self, Request, StatusCode},
-    Router,
 };
-use sqlx::{Connection, Executor, PgConnection, PgPool};
 use tower::{Service, ServiceExt};
-use uuid::Uuid;
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
-pub struct TestApp {
-    pub app: Router,
-    pub db_pool: PgPool,
-}
+mod common;
 
-pub async fn setup_app() -> TestApp {
-    let mut configuration = get_configuration().expect("Failed to read configuration.yml");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
-
-    let app = zero2prod::startup::create_app(connection_pool.clone());
-
-    TestApp {
-        app,
-        db_pool: connection_pool,
-    }
-}
-
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    // データベースを作成する
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-        .await
-        .expect("Failed to create database.");
-
-    // データベースに対してマイグレーションを実行する
-    let connection_pool = PgPool::connect(&config.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-
-    sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .expect("Failed to migrate the database");
-
-    connection_pool
-}
+use common::setup_app;
 
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_from_data() {
