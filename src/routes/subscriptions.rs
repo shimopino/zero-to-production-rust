c1cdae4f9f16a4576030b2d89eb6f4ca49b6569b
shimopino <shimoke4869@gmail.com>
2023-04-20 21:56:19 +0900

    feat: 名前の検証処理を追加

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Form};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
@@ -14,7 +15,9 @@ pub async fn subscribe(
     State(pool): State<PgPool>,
     Form(input): Form<Subscribe>,
 ) -> impl IntoResponse {
-    println!("{}, {}", input.name, input.email);
+    if !is_valid_name(&input.name) {
+        return StatusCode::BAD_REQUEST;
+    }
 
     match sqlx::query!(
         r#"
@@ -35,4 +38,15 @@ pub async fn subscribe(
             StatusCode::INTERNAL_SERVER_ERROR
         }
     }
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
