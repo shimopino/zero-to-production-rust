dc87252f719f8a0ee4699f4245849db43455172d
shimopino <shimoke4869@gmail.com>
2023-04-23 17:41:56 +0900

    feat: instrumentアトリビュートを使用したログ出力を追加

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Form};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
@@ -23,45 +22,38 @@ impl TryFrom<Subscribe> for NewSubscriber {
     }
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    )
+)]
 pub async fn subscribe(
     State(pool): State<PgPool>,
     Form(form): Form<Subscribe>,
 ) -> impl IntoResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name,
-    );
-    let _request_span_guard = request_span.enter();
-
     let new_subscriber = match form.try_into() {
         Ok(subscriber) => subscriber,
         Err(_) => return StatusCode::BAD_REQUEST,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            StatusCode::CREATED
-        }
-        Err(e) => {
-            tracing::error!("request_id {} - Failed to execute query: {}", request_id, e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
+        Ok(_) => StatusCode::CREATED,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(new_subscriber, pool)
+)]
 pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -73,8 +65,11 @@ pub async fn insert_subscriber(
         Utc::now()
     )
     .execute(pool)
-    .instrument(query_span)
-    .await?;
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query {:?}", e);
+        e
+    })?;
 
     Ok(())
 }
