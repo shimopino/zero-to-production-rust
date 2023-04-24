8f8a3f107fe93ac00657117fe531aa83dce8fe00
shimopino <shimoke4869@gmail.com>
2023-04-24 20:05:16 +0900

    feat: DB接続時にタイムアウトを設定

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
     startup::create_app,
@@ -12,9 +12,10 @@ async fn main() {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect database");
 
     // 実行する
     let addr = format!(
