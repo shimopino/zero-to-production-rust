45c4ca9c6fd28d6f29317f6c2024db0fb99aa7c1
shimopino <shimoke4869@gmail.com>
2023-04-27 05:32:57 +0900

    feat: EmailClientのテストを追加

--- src/email_client.rs
+++ src/email_client.rs
@@ -31,8 +31,41 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
+    use crate::email_client::EmailClient;
+    use fake::faker::lorem::en::Paragraph;
+    use fake::faker::{internet::en::SafeEmail, lorem::en::Sentence};
+    use fake::Fake;
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::domain::SubscriberEmail;
+
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
-        todo!()
+        // Arrange
+        // ランダムなポートを使用してバックグラウンドでサーバーを起動する
+        // uriメソッドでURLを取得可能
+        let mock_server = MockServer::start().await;
+        let fake_email = SafeEmail().fake();
+        let sender = SubscriberEmail::parse(fake_email).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        // Mockサーバーからのレスポンスのモックを設定する
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            // 条件と一致するリクエストを1つだけ受け取ることができる
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject = Sentence(1..2).fake::<String>();
+        let content = Paragraph(1..10).fake::<String>();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
     }
 }
