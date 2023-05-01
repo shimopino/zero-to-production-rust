d05f9307a35be0fc055ea386c27f8c3d945d4f44
shimopino <shimoke4869@gmail.com>
2023-05-01 12:23:10 +0900

    test: クエリパラメータに設定したトークンを使って検証を行うテストを追加

--- tests/api/subscription_confirm.rs
+++ tests/api/subscription_confirm.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 use axum::{
     body::Body,
     http::{self, Request, StatusCode},
 };
+use reqwest::Url;
 use tower::ServiceExt;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -89,4 +92,55 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     // 2つのリンクが同じであることを確認する
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
+}
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let mut test_app = setup_app().await;
+    let body = "name=shimopino&email=shimopino%40example.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscription(body.into()).await;
+
+    let email_request = &test_app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = test_app.get_confirmation_links(email_request);
+
+    let query_params = extract_query_params(&confirmation_links.html);
+    let token = query_params.get("subscription_token").unwrap();
+
+    // Act
+    let query = format!("subscription_token={}", token);
+    test_app
+        .app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::GET)
+                .uri(format!("/subscriptions/confirm?{}", query))
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "shimopino@example.com");
+    assert_eq!(saved.name, "shimopino");
+    assert_eq!(saved.status, "confirmed");
+}
+
+fn extract_query_params(url: &Url) -> HashMap<String, String> {
+    url.query_pairs()
+        .map(|(key, value)| (key.to_string(), value.to_string()))
+        .collect()
 }
