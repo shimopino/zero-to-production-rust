3ad5030d3f1713378c9c5d92576e200852dab425
shimopino <shimoke4869@gmail.com>
2023-05-03 14:07:02 +0900

    feat: エンドポイントがJSON形式のリクエストを受け付けるように修正

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,19 @@
-use axum::response::IntoResponse;
+use axum::{response::IntoResponse, Json};
 use hyper::StatusCode;
+use serde::Deserialize;
 
-pub async fn publish_subscriber() -> impl IntoResponse {
+#[derive(Debug, Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+pub async fn publish_subscriber(Json(body): Json<BodyData>) -> impl IntoResponse {
     StatusCode::OK
 }
