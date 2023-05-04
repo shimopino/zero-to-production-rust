c6d6995bf9e5d2fbc5fab617d7ef59c9e291f0c3
shimopino <shimoke4869@gmail.com>
2023-05-04 19:28:01 +0900

    feat: ログイン機能を追加

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,9 +1,17 @@
 use axum::{
+    extract::State,
+    http::StatusCode,
     response::{IntoResponse, Redirect},
     Form,
 };
 use secrecy::Secret;
 use serde::Deserialize;
+
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    error::error_chain_fmt,
+    startup::AppState,
+};
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -11,7 +19,56 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(Form(form): Form<FormData>) -> impl IntoResponse {
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl IntoResponse for LoginError {
+    fn into_response(self) -> axum::response::Response {
+        let status_code = match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        };
+
+        status_code.into_response()
+    }
+}
+
+#[tracing::instrument(
+    name = "login",
+    skip(form, state),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    State(state): State<AppState>,
+    Form(form): Form<FormData>,
+) -> Result<impl IntoResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.username,
+        password: form.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentials(credentials, &state.db_state.db_pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
     // https://docs.rs/axum/latest/axum/response/struct.Redirect.html#method.to
-    Redirect::to("/")
+    Ok(Redirect::to("/"))
 }
